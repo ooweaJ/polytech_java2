04fca412a639f4d213c5b2165418377c70ab917d
ooweaJ <dbwidn2@naver.com>
2026-06-08 10:30:01 +0900

    Java 기본기 연습

---
+++ day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
@@ -0,0 +1,11 @@
+package com.survivalcoding;
+
+public class MyFirstClass {
+    public static void main(String[] args)
+    {
+        final int i = 0;
+
+        // sout
+        System.out.println("헬로!!!!");
+    }
+}
